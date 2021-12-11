4b9cbb87fba6fea050a31a7383bdc1e8d04c47cf
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-12-11 23:48:43 +0530

    minor bugs

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,20 +4,24 @@ module.exports = {
     title: "spam-king",
   },
   plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Spam King`,
         short_name: `spam`,
+        description:"Spam King Official Website",
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
+        display: `standalone`,
+        lang:'en',
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },}
+      }
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
   ],
 };
