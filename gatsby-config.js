2dbf9fdab5fb2ff27dcc609429d8dc735ea0ee9a
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-12-11 22:47:54 +0530

    init

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "spam-king",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
